af76c473e97db468a0838dcc42124cabe7fd5892
Mohamed ELkashif <Mohamed ELkashif>
2017-01-10 03:06:38 +0200

    vagrant

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+# sudo apt-get install -y  python g++ make
+# sudo add-apt-repository -y ppa:chris-lea/node.js
+# sudo apt-get update
+# sudo apt-get install -y  nodejs
+
+# npm install redis
+
+
+ sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs-legacy
+
+sudo apt install npm
+
